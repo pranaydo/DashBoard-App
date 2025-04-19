f8190eaff77ff65f3b408f107699562df0b74c29
Pranay <pranaydodake7@gmail.com>
2025-04-19 21:54:29 +0530

    Folder stucture improved tob bar component moved to top for global use

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,32 @@ import { Container, Tabs, Tab, Box } from "@mui/material";
 import MetricsView from "./components/view/MetricView";
 import AnalyticsView from "./components/view/AnalyticsView";
 import { UserProvider } from "./context/UserContext";
+import TopBar from "./components/TopBar";
+import FiltersPanel from "./components/FilterPanel";
+import { Filters } from "./type/types";
 
 const App: React.FC = () => {
   const [tab, setTab] = useState<number>(0);
+  const [filters, setFilters] = useState<Filters>({});
 
   console.log(tab);
   return (
     <UserProvider>
       <div className="App">
         <Container>
-          <Tabs value={tab} onChange={(e, newVal) => setTab(newVal)}>
-            <Tab label="Metrics View" />
-            <Tab label="Analytics View" />
-          </Tabs>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <Tabs value={tab} onChange={(e, newVal) => setTab(newVal)}>
+              <Tab label="Metrics View" />
+              <Tab label="Analytics View" />
+            </Tabs>
+            <div>
+              <TopBar />
+            </div>
+          </div>
+          <div style={{ border: "1px solid red", marginTop: "2px" }}>
+            <FiltersPanel filters={filters} setFilters={setFilters} />
+          </div>
+
           <Box mt={2}>{tab === 0 ? <MetricsView /> : <AnalyticsView />}</Box>
         </Container>
       </div>
--- src/components/view/MetricView.tsx
+++ src/components/view/MetricView.tsx
@@ -1,15 +1,9 @@
-import { useState } from "react";
 import { Filters } from "../../type/types";
-import TopBar from "../TopBar";
 import MetricsTable from "./MetricsTable";
-import FiltersPanel from "../FilterPanel";
-const MetricsView = () => {
-  const [filters, setFilters] = useState<Filters>({});
 
+const MetricsView: React.FC<Filters> = (filters) => {
   return (
     <div className="metric-top-bar">
-      <TopBar />
-      {/* <FiltersPanel filters={filters} setFilters={setFilters  } />  bugs are there need to fix */}
       <MetricsTable filters={filters} />
     </div>
   );
