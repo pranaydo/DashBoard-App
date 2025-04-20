05cd55fa8292c2c9c44943fc67e49b5632b7079f
Pranay <pranaydodake7@gmail.com>
2025-04-20 18:28:48 +0530

    tob bar enhancement user name is showing ion the top

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,16 +1,19 @@
 import { Button, Dialog, DialogTitle, ButtonGroup, Box } from "@mui/material";
-import { useState } from "react";
+import React, { useState } from "react";
 import { users } from "../mockData/MockData";
 import { User } from "../type/types";
 import { useUser } from "../context/UserContext";
 const TopBar: React.FC = () => {
+  const { user, setUser } = useUser();
   const [open, setOpen] = useState<boolean>(false);
-  // const [currentUser, setCurrentUser] = useState<string>("");
-  const { setUser } = useUser();
+  const [currentUser, setCurrentUser] = useState<string>(user.name);
+
+  // console.log(user);
 
   return (
     <div>
       <div style={{ display: "flex", justifyContent: "flex-end" }}>
+        {currentUser}
         <Button
           style={{ marginTop: "5px" }}
           variant="contained"
@@ -19,44 +22,42 @@ const TopBar: React.FC = () => {
           My Members
         </Button>
         {/* {currentUser} Will show current user in something like avtar  */}
-
         <Dialog
           open={open}
           onClose={() => setOpen(false)}
           fullWidth={true}
-          maxWidth="md"
+          maxWidth="sm"
         >
           <DialogTitle>Select a User</DialogTitle>
           <Box
             sx={{
               display: "flex",
-              "& > *": {
-                m: 1,
-              },
+              flexDirection: "column",
+              padding: 2,
+              gap: 1,
             }}
           >
-            <ButtonGroup
-              orientation="vertical"
-              fullWidth
-              variant="contained"
-              sx={{ padding: 2 }}
-            >
-              {users.map((user: User) => (
-                <Button
-                  style={{ paddingRight: "5px" }}
-                  key={user.id}
-                  onClick={() => {
-                    console.log("Selected user data:", user);
-                    // setCurrentUser(user.name);
-                    setUser(user);
-                    setOpen(false);
-                  }}
-                  sx={{ justifyContent: "center", textTransform: "none" }}
-                >
-                  {user.name}
-                </Button>
-              ))}
-            </ButtonGroup>
+            {users.map((user: User) => (
+              <Button
+                key={user.id}
+                onClick={() => {
+                  console.log("Selected user data:", user);
+                  setCurrentUser(user.name);
+                  setUser(user);
+                  setOpen(false);
+                }}
+                variant="contained"
+                size="small"
+                sx={{
+                  justifyContent: "center",
+                  textTransform: "none",
+                  width: "200px",
+                  alignSelf: "center",
+                }}
+              >
+                {user.name}
+              </Button>
+            ))}
           </Box>
         </Dialog>
       </div>
